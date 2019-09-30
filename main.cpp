93f02e9f7762fd9176237c34a321eb90308fe28d
cnmullins <cnmullins@mail.csuchico.edu>
2019-09-30 16:34:44 -0700

    Now reads simple text files

--- main.cpp
+++ main.cpp
@@ -14,17 +14,23 @@ int main()
   getline(cin, fileName);
   
   //STEP 1: open the fileStream for input, using the fileName specified
+  fileStream.open(fileName.c_str(), std::fstream::in);
   
-  if( /*STEP 2: check to see if the fileStream successfully opened*/ )
+  /*STEP 2: check to see if the fileStream successfully opened*/
+  if(fileStream.good())
   {
     cout<<fileName<<" opened.\nFILE CONTENTS:\n";
-    
+    do
+    {
+      getline(fileStream, line);      
+      cout << line << endl;
+    } while (!fileStream.eof());
     //STEP 3: repeat the following until the end-of-file (eof) has been reached...
     // 3A: read a line from fileStream into the variable line
     // 3B: display the line, followed by an endline
   
     //STEP 4: close the fileStream
-  
+    fileStream.close();
   }
   else
   {
